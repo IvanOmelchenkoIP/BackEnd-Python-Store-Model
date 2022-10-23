ab533cd9e671e93e6938c7a89ad132300013954c
IvanOmelchenkoIP <io25112002@gmail.com>
2022-10-23 15:53:04 +0300

    Add functions to views.py
    Add functions that will implement program logic to views.py

---
+++ backend/views.py
@@ -0,0 +1,17 @@
+def create_user():
+    pass
+
+def create_category():
+    pass
+
+def create_record():
+    pass
+
+def get_categories():
+    pass
+
+def get_records_by_user():
+    pass
+
+def get_records_by_category():
+    pass

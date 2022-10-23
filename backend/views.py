ea5d8cc35c9036cdacbb6e5d8124d1e5bd7958a5
IvanOmelchenkoIP <io25112002@gmail.com>
2022-10-23 19:01:59 +0300

    Implement create_category and create_record
    Implement create_category and create_record in a similar way to
    create_user function in views.py

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from unicodedata import name
 import uuid
 from flask import jsonify, request
@@ -11,21 +12,38 @@ records = []
 def create_user():
     user_data = request.get_json()
     user_id = uuid.uuid4()
-    user = {"id": user_id, "user_name": user_data["user_name"]}
+    user = {
+        "id": user_id, 
+        "user_name": user_data["user_name"]
+        }
     users.append(user)
     return jsonify(user)
 
 @app.post("/newcategory")
 def create_category():
     category_data = request.get_json()
-    categories.append(category_data)
-    return "OK"
+    category_id = uuid.uuid4()
+    category = {
+        "id": category_id, 
+        "category_name": category_data["category_name"]
+        }
+    categories.append(category)
+    return jsonify(category)
 
 @app.post("/newrecord")
 def create_record():
     record_data = request.get_json()
-    records.append(record_data)
-    return "OK"
+    record_id = uuid.uuid4()
+    time = datetime.now()
+    record = {
+        "id": record_id, 
+        "user_id": record_data["user_id"], 
+        "category_id": record_data["category_id"], 
+        "time": time, 
+        "sum": record_data["sum"]
+        }
+    records.append(record)
+    return jsonify(record)
 
 @app.route("/categories")
 def get_categories():

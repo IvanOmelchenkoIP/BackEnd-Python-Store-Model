666453d36ffff127379cec72e6a3cd7dfad4810c
IvanOmelchenkoIP <io25112002@gmail.com>
2022-10-23 17:22:16 +0300

    Add routing to views.py
    Add routing to each function in views.py

--- backend/views.py
+++ backend/views.py
@@ -1,17 +1,25 @@
+from backend import app
+
+@app.route("/newuser", methods = ["POST"])
 def create_user():
     pass
 
+@app.route("/newcategory", methods = ["POST"])
 def create_category():
     pass
 
+@app.route("/newrecord", methods = ["POST"])
 def create_record():
     pass
 
+@app.route("/categories")
 def get_categories():
     pass
 
+@app.route("/userrecords")
 def get_records_by_user():
     pass
 
+@app.route("/categoryrecords")
 def get_records_by_category():
     pass

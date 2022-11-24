8a1b0543705987601686896d9d937d100c5f64d9
IvanOmelchenkoIP <io25112002@gmail.com>
2022-11-24 19:04:37 +0200

    Add blueprint for users
    Add blueprint for users for adding new user and getting user list

---
+++ backend/resources/users.py
@@ -0,0 +1,18 @@
+from flask import MethodView, jsonify, request
+from flask_smorest import Blueprint, abort
+
+from backend.storages import users
+
+blp = Blueprint("user", __name__, description = "Blueprint for operations on users")
+
+@blp.route("/user")
+class Users(MethodView):
+    def post(self):
+        user_res = users.add(request.get_json())
+        if "err" in user_res:
+            abort(400, user_res["err"])
+        res = {"status": "OK", "user": user_res}
+        return jsonify(res)
+
+    def get(self):
+        return jsonify(users.get_users())

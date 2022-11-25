40298ec8fcdb107dc762e76b8e4f5a79a9fdddcc
IvanOmelchenkoIP <io25112002@gmail.com>
2022-11-25 12:15:16 +0200

    Init db in views
    Init databases with app in views.py

--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,8 @@
 from flask_smorest import Api
 from backend import app
 
+from backend.models.db import db
+
 from backend.resources.users import blp as UsersBlueprint
 from backend.resources.categories import blp as CategoriesBlueprint
 from backend.resources.records import blp as RecordsBlueprint
@@ -9,8 +11,14 @@ app.config["PROPAGATE_EXCEPTIONS"] = True
 app.config["API_TITLE"] = "Backend Labs"
 app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_VERSION"] = "3.0.2"
+app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 api = Api(app)
+
+with app.app_context():
+    db.create_all()
+
 api.register_blueprint(UsersBlueprint)
 api.register_blueprint(CategoriesBlueprint)
 api.register_blueprint(RecordsBlueprint)
